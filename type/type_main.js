58d20526df7ab78ae0ebbac1c25d993008399160
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2020-10-02 04:26:19 -0500

    extract single trimmed glyph from grid

--- type/type_main.js
+++ type/type_main.js
@@ -9,34 +9,89 @@ const ctx = canvas.getContext('2d');
 window.addEventListener('load', init);
 
 function init(e) {
-    canvas.height = 257;
-    canvas.width = 257;
-    convertPngToText();
+    paintPngToCanvas();
     // Configure bitmap selector
     imgSelect.addEventListener('change', e => {
-        bitmap.onload = convertPngToText;
+        bitmap.onload = paintPngToCanvas;
         bitmap.src = e.target.value;
         imgSelect.blur();
     });
+    // Configure canvas click detector
+    canvas.addEventListener('click', doCanvasClick);
 }
 
-function convertPngToText() {
+function paintPngToCanvas() {
     let h = bitmap.height;
     let w = bitmap.width;
     canvas.height = h;
     canvas.width = w;
-    ctx.drawImage(bitmap, 0, 0);
-    var idat = ctx.getImageData(0, 0, w, h);
-    // Convert image data to text pixels
-    let row_buf = [];
-    for (let y=0; y<idat.height; y++) {
-        let row = [];
-        for (let x=0; x<idat.width; x++) {
-            let offset = ((idat.width * y) + x) * 4;
-            let r = idat.data[offset];
-            row.push(r>0 ? "." : '#');
-        }
-        row_buf.push(row.join(""));
+    ctx.drawImage(bitmap, 0, 0, w, h);
+    preOut.textContent = "";
+}
+
+function doCanvasClick(e) {
+    let bbox = e.target.getBoundingClientRect();
+    let x = e.clientX - Math.floor(bbox.left);
+    let y = e.clientY - Math.floor(bbox.top);
+    // Grid is 16x16 boxes with fixed 1px gutter and border
+    let gridSize = Math.floor((canvas.width - 1) / 16);
+    // Determine which grid box contains the click coordinate
+    let row = Math.floor(y/gridSize);
+    let col = Math.floor(x/gridSize);
+    convertGlyphBoxToText(row, col);
+}
+
+function convertGlyphBoxToText(row, col) {
+    if (row < 0 || row > 15 || col < 0 || col > 15) {
+        // Ignore clicks on grid border, etc.
+        return;
     }
-    preOut.textContent = row_buf.join("\n");
+    let w = canvas.width;
+    let h = canvas.height;
+    var idat = ctx.getImageData(0, 0, w, h);
+    // Get pixels for grid cell, converting from RGBA to 1-bit
+    let grid = Math.floor((w - 1) / 16);
+    let pxMtrx = [];
+    for (let y=(row*grid)+1; y<(row+1)*grid; y++) {
+        let row = [];
+        for (let x=(col*grid)+1; x<(col+1)*grid; x++) {
+            let offset = ((w * y) + x) * 4;
+            let r = idat.data[offset];
+            row.push(r>0 ? 0 : 1);
+        }
+        pxMtrx.push(row);
+    }
+    // Trim left whitespace
+    pxMtrx = matrixTranspose(pxMtrx);
+    while (pxMtrx.length>0 && pxMtrx[0].reduce((a, b) => a+b) == 0) {
+        pxMtrx.shift();
+    }
+    // Trim right whitespace
+    pxMtrx.reverse();
+    while (pxMtrx.length>0 && pxMtrx[0].reduce((a, b) => a+b) == 0) {
+        pxMtrx.shift();
+    }
+    pxMtrx.reverse();
+    pxMtrx = matrixTranspose(pxMtrx);
+    // Show trimmed result as one ASCII char per pixel
+    preOut.textContent = pxMtrx.map(
+        row => row.map(col => col==0 ? "." : "#").join("")
+    ).join("\n");
+}
+
+function matrixTranspose(matrix) {
+    if (matrix.length < 1) {
+        return matrix;
+    }
+    let w = matrix[0].length;
+    let h = matrix.length;
+    let transposed = [];
+    for (let col=0; col<w; col++) {
+        let trRow = [];
+        for (let row=0; row<h; row++) {
+            trRow.push(matrix[row][col]);
+        }
+        transposed.push(trRow);
+    }
+    return transposed;
 }
