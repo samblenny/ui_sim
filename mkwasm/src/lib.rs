85afda20f208a08cdd95b87989ddc6a065e895c4
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2020-10-19 17:26:26 -0500

    rename gui crate to guilib

--- mkwasm/src/lib.rs
+++ mkwasm/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-extern crate gui;
+extern crate guilib;
 extern crate trace;
 
 /// For building wasm32 no_std, add panic handler and functions to let
@@ -11,49 +11,49 @@ pub mod no_std_bindings;
 /// Initialize the hardware (splash screen, etc.)
 #[no_mangle]
 pub extern "C" fn init() {
-    gui::api::repaint();
+    guilib::api::repaint();
 }
 
 /// Check if lcd frame buffer is dirty: 0=>clean, _=>dirty
 #[no_mangle]
 pub extern "C" fn lcd_dirty() -> i32 {
-    gui::api::lcd_dirty() as i32
+    guilib::api::lcd_dirty() as i32
 }
 
 /// Mark lcd frame buffer as clean
 #[no_mangle]
 pub extern "C" fn lcd_clear_dirty() {
-    gui::api::lcd_clear_dirty();
+    guilib::api::lcd_clear_dirty();
 }
 
 /// Step the UI demo animation
 #[no_mangle]
 pub extern "C" fn demo_tick() {
-    gui::api::demo_tick();
+    guilib::api::demo_tick();
 }
 
 /// Respond to key press event
 #[no_mangle]
 pub extern "C" fn keydown(key_index: i32) {
-    gui::api::keydown(key_index as u32);
+    guilib::api::keydown(key_index as u32);
 }
 
 /// Respond to key release event
 #[no_mangle]
 pub extern "C" fn keyup(key_index: i32) {
-    gui::api::keyup(key_index as u32);
+    guilib::api::keyup(key_index as u32);
 }
 
 /// Substitute for using (non-existant) menu to select azerty keyboard layout
 #[no_mangle]
 pub extern "C" fn set_layout_azerty() {
-    gui::api::kbd_set_layout_azerty();
+    guilib::api::kbd_set_layout_azerty();
 }
 
 /// Substitute for using (non-existant) menu to select qwerty keyboard layout
 #[no_mangle]
 pub extern "C" fn set_layout_qwerty() {
-    gui::api::kbd_set_layout_qwerty();
+    guilib::api::kbd_set_layout_qwerty();
 }
 
 #[cfg(test)]
--- mkwasm/src/no_std_bindings.rs
+++ mkwasm/src/no_std_bindings.rs
@@ -16,22 +16,22 @@ pub fn panic(_panic_info: &PanicInfo) -> ! {
     }
 }
 
-extern crate gui;
+extern crate guilib;
 
 /// Export pointer and size of shared frame buffer for javascript
 #[no_mangle]
 pub unsafe extern "C" fn lcd_words_per_line() -> i32 {
-    gui::api_wasm::lcd_words_per_line() as i32
+    guilib::api_wasm::lcd_words_per_line() as i32
 }
 #[no_mangle]
 pub unsafe extern "C" fn lcd_px_per_line() -> i32 {
-    gui::api_wasm::lcd_px_per_line() as i32
+    guilib::api_wasm::lcd_px_per_line() as i32
 }
 #[no_mangle]
 pub unsafe extern "C" fn lcd_lines() -> i32 {
-    gui::api_wasm::lcd_lines() as i32
+    guilib::api_wasm::lcd_lines() as i32
 }
 #[no_mangle]
 pub unsafe extern "C" fn lcd_frame_buf_ptr() -> *const u32 {
-    gui::api_wasm::lcd_frame_buf_ptr()
+    guilib::api_wasm::lcd_frame_buf_ptr()
 }
